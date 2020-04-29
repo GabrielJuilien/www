3becafd8d07b064b0d283cdf9ec1490bd46251ee
Mathis <grandemange@et.esiea.fr>
2020-04-29 17:04:44 +0100

    operator request transfert

--- editingRequest.php
+++ editingRequest.php
@@ -24,7 +24,7 @@ session_start();
 		LEFT JOIN problems WHERE          relations.ID_problem = problem.ID_problem
 		LEFT JOIN departments WHERE       employees.ID_Department = departments.ID_Department
 		LEFT JOIN jobs WHERE              employees.ID_Job = jobs.ID_Job");
-		$requete->execute(array(":id"=>$_GET['ID_Request']));
+		$requete->execute(array(":id"=>$_GET['tickets']));
 		$res=$requete->fetch();?>
 
         <form>
--- operatorRequestTransfert.php
+++ operatorRequestTransfert.php
@@ -12,25 +12,21 @@ if (!isset($_SESSION['user_id'])) {
 <body>	
 		
 		<?php
-		$bdd=	new PDO("mysql:host=127.0.0.1;dbname=helpdesk",'helpdesk_default','xixn2lCbJe90Xa8n');//id et mdp tmp
-		$requete = $bdd->prepare("SELECT employees.First_Name, employees.Last_Name, jobs.Job_Name FROM employees ");
-		$requete->bindParam(1,$_GET['ID_Request']);
-		$requete->execute();
-		$res=$requete->fetch();?>
-		<div class = "conteneurticket">
-		    <?php echo $res["Submission_Datetime"] ?>
-            <?php echo $res["Processing_Datetime"] ?>
-            <?php echo $res["Solve_Datetime"] ?>
-			<?php echo $res["Problem_Description"] ?>
-			<?php echo $res["Solution_Description"] ?>
-			<?php echo $res["Device_Details"] ?>
-			<?php echo $res["Device_Type_Name"] ?>
-			<?php echo $res["First_Name"] ?>
-			<?php echo $res["Last_Name"] ?>
-			<?php echo $res["Department_Name"] ?>
-			<?php echo $res["Job_Name"] ?>
-        </div>
-		 <a href="editingRequest.php?ticket="<?php echo $_GET['ID_Request']?>>edit ticket</a>	
+			$bdd=	new PDO("mysql:host=127.0.0.1;dbname=helpdesk",'helpdesk_default','xixn2lCbJe90Xa8n');//id et mdp tmp
+			$requete = $bdd->prepare("SELECT employees.First_Name, employees.Last_Name, specialities.Speciality_Name FROM specialization WHERE employees.ID_Job =3 
+				LEFT JOIN employees ON employees.ID_Employee = specialization.ID_Specialist
+				LEFT JOIN jobs ON jobs.ID_Job=employees.ID_Job
+				LEFT JOIN specialities ON specialities.ID_Speciality = specialization.ID_Speciality");
+			$requete->execute();
+			 while ($resultat = $requete->fetch()) {?>
+				<div class = "liste specialiste">
+				    <?php echo "specialist name : ".$resultat["First_Name"].$resultat[Last_Name]?>
+				    <?php echo "speciality : ".$resultat["Speciality_Name"] ?>
+
+				    <a href="createTask.php?ticket=<?php echo $_GET['ticket']?>&specialist=<?php echo $resultat['ID_Specialist'] ?>"> create task</a>
+		        </div>
+			 	
+		<?php } ?>
 </body>
 
 </html>
